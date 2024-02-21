48acc6fc1c31e47042904af7ac0d67dc1f26d35e
Ntlele <ntleledavid07@gmail.com>
2024-02-21 17:16:10 +0000

    c program that prints text

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	/*
-	 *The c program that prints Programming is like building a multilingual puzzle
-	 */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
