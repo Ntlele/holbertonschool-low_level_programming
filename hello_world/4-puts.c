9db7916bd8d100f2adc6706a4d994133f7919023
Ntlele <ntleledavid07@gmail.com>
2024-02-21 17:13:36 +0000

    c program that prints text

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,8 +3,8 @@
 int main(void)
 {
 	/*
-	 * The c program that prints \"Programming is like building a multilingual puzzle
-	 * */
+	 *The c program that prints Programming is like building a multilingual puzzle
+	 */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
