b44cdb9758c37969c835a44324afaa3c3f73c54f
Ntlele <ntleledavid07@gmail.com>
2024-02-22 04:40:17 +0000

    c program that prints the size of various types

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,7 +5,7 @@
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
 	printf("Size of a char: %ld byte(s)\n", sizeof(char));
 	printf("Size of an int: %ld byte(s)\n", sizeof(int));
