6d9849b1aab7bcda76339a49c0a84c04c6c969f9
Ntlele <ntleledavid07@gmail.com>
2024-02-22 16:15:43 +0000

    A funtion that prints different numbers and tells if they are positive or negative or zero

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/*
- * more headers goes there
+/**
+ * main - Entry point
  *
- * betty style doc for function main goes there
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
