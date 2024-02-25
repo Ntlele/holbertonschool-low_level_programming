46873913bdfb74c6cbd34cd5307019465a364ea7
Ntlele <ntleledavid07@gmail.com>
2024-02-25 10:06:44 +0000

    using a prototype to print out alphabets

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphhabert(void);
+
+int main(void)
+{
+	print_alphabet();
+	_putchar('\n');
+	return (0);
+}
+
+void print_alphhabert(void)
+{
+	char al;
+
+	for (al = 'a';la <= 'z';al++)
+	{
+	_putchar(al);
+	}
+}
+ 
