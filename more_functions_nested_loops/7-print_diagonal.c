5a7e8e13ec24af8eb0cfc6820afa5af101e13233
Ntlele <ntleledavid07@gmail.com>
2024-02-26 19:01:05 +0000

    in progress

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_diagonal - draws a diagonal line
+ * @n: number of times to print
+ * Return: void
+ *
+ */
+void print_diagonal(int n)
+{
+	if (n > 0)
+	{
+		int _slsh, _spc;
+
+		for (_spc = 0; _spc < n; _spc++)
+		{
+			for (slsh = 0; slsh < n; slsh++)
+			{
+				_putchar();
+			}
+		}
+	}
+}
