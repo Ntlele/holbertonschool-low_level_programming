5c974c6bff3c8690da7175f33ab800847c479552
Ntlele <ntleledavid07@gmail.com>
2024-02-27 16:36:26 +0000

    funtion that prints a square

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,30 +1,28 @@
 #include "main.h"
 
 /**
- * print_square - prints out a square
- * @size: the size of the square
- *
- * Return: void
+ * print_square - print a square
+ * @size: stores the size of square
+ * Description: should only use '#' to print square.
  */
-
 void print_square(int size)
 {
-	int h, w;
+	int w, h;
 
-	w = 0;
-	h = w;
+	y = 0;
 
-	if (size <= 0)
+	if (size < 1)
 		_putchar('\n');
-	else
-		while (h < size)
+
+	while (h < size)
+	{
+		w = 0;
+		while (w < size)
 		{
-			while (w < size)
-			{
-				_putchar('#');
-				w++;
-			}
-			_putchar('\n');
-			h++;
+			_putchar('#');
+			w++;
 		}
+		_putchar('\n');
+		h++;
+	}
 }
