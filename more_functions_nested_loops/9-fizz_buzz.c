1004d2beefd278562bf40fb789e849aea85293a4
Ntlele <ntleledavid07@gmail.com>
2024-02-28 05:06:28 +0000

    replacing multiples of 3 with fizz and multiples of 5 with buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,37 +1,29 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - entry point
+ * Return: exit point
  */
 
-int main()
+int main(void)
 {
 	int num;
-	
+
 	for (num = 1; num <= 100; num++)
 	{
-		if(num % 3 == 0 && num % 5 == 0)
-		{
+		if ((num % 3) == 0 && (num % 5) == 0)
 			printf("FizzBuzz");
-		}
-		else if (num % 3 == 0)
-		{
+		else if  ((num % 3) == 0)
 			printf("Fizz");
-		}
-		else if (num % 5 == 0)
-		{
+		else if ((num % 5) == 0)
 			printf("Buzz");
-		}
 		else
-		{
 			printf("%d", num);
-		}
 
 		if (num == 100)
 			continue;
-		printf(" ");	
+		printf(" ");
 	}
+	printf("\n");
 	return (0);
 }
