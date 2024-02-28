c2521873ef812a82f58454f224a1de03e63ab978
Ntlele <ntleledavid07@gmail.com>
2024-02-28 16:49:08 +0000

    updating an int pointer value to 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates the value of a pointer to 98
+ * @*n: stores the pointer to an integer
+ * Returns: void
+ *
+ */
+void reset_pointer_to_98(int *n)
+{
+	int num;
+	
+	*n = &num;
+
+	*n = 98;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * reset_to_98 - updates an int pointer to 98
+ * @*n: stores the value of a pointer
+ *
+ */
+void reset_to_98(int *n;
+#endif
