788bae790df05c3e817b9f5c9380539ca8c99818
Ntlele <ntleledavid07@gmail.com>
2024-03-02 10:51:51 +0000

    this is a function that links two strings together

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -11,15 +11,17 @@ char *_strcat(char *dest, char *src)
 {
 	int p, i;
 
+	i = 0;
 	while (dest[i] != '\0')
 		i++;
 
+	p = 0;
 	while (src[p] != '\0')
 	{
 		dest[i] = src[p];
 		i++;
 		p++;
 	}
-	dest[i] != '\0'
+	dest[i] != '\0';
 	return (dest);
 }
