07b47bd44c800ad94668af88966ff87d4da065a8
Ntlele <ntleledavid07@gmail.com>
2024-03-02 14:49:47 +0000

    function that copies a string type variable

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -14,12 +14,10 @@ char *_strncpy(char *dest, char *src, int n)
 	while (src[i] != '\0' && i < n)
 	{
 		dest[i] = src[i];
-		i++;
 	}
 	while (i < n)
 	{
 		dest[i] = '\n';
-		i++;
 	}
 	return (dest);
 }
