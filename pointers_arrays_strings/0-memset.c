7228164f1ca4b344cba0aca5603e37a995cb4cee
Ntlele <ntleledavid07@gmail.com>
2024-03-05 05:24:28 +0000

    function that fills memory area with constant byte

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - a function that fills memory with constant byte
+ * @s: memory area
+ * @b: constant byte
+ * @n: number of bytes
+ * Return: a pointer to the memory area
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -166,4 +166,18 @@ char *leet(char *n);
  * Return: the resulting string
  */
 char *rot13(char *);
+
+
+/**
+ * Even more pointers arrays and strings
+ */
+
+/**
+ * _memset - a function that fills memory with constant byte
+ * @s: memory area
+ * @b: constant byte
+ * @n: number of bytes
+ * Return: a pointer to the memory area
+ */
+char *_memset(char *s, char b, int n);
 #endif
