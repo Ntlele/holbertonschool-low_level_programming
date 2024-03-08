b3bf75258bb55032d22754019fd06dc05a229470
Ntlele <ntleledavid07@gmail.com>
2024-03-08 18:17:21 +0000

    function that returns the value of x raised to the power of y

--- recursion/main.h
+++ recursion/main.h
@@ -43,4 +43,12 @@ int _strlen_recursion(char *s);
  */
 
 int factorial(int n);
+
+/**
+ * _pow_recursion - returns the value of x raised to the power of y.
+ * @x: base number.
+ * @y: pow number.
+ * Return: power number.
+ */
+int _pow_recursion(int x, int y);
 #endif
