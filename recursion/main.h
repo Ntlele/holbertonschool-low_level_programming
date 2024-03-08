2c00188471802beb45010885fadf16bcc0ebf6db
Ntlele <ntleledavid07@gmail.com>
2024-03-08 05:36:03 +0000

    function that prints a string in reverse

--- recursion/main.h
+++ recursion/main.h
@@ -18,4 +18,12 @@ void _puts_recursion(char *s);
  */
 int _putchar(char c);
 
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: pointer to the string printed
+ * Return: void
+ *
+ */
+
+void _print_rev_recursion(char *s);
 #endif
