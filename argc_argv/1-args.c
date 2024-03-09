71e50a5d6e6e2284a9faddb8df685c62a5adbd55
Ntlele <ntleledavid07@gmail.com>
2024-03-09 10:26:04 +0000

    program that prints all arguments it receives

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints out the number of arguments
+ * @argc: store the number of programs
+ * @argv: stores the names of programs
+ * Return: always 0
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum;
+
+	sum = argc - 1;
+	printf("%d\n", sum);
+	return (0);
+}
