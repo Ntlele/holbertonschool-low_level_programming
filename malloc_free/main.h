8f65933baf826abd37e862c05ee3821612115aa6
Ntlele <ntleledavid07@gmail.com>
2024-03-11 15:36:07 +0000

    function that returns a pointer to a newly allocated space in memory

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -10,4 +10,14 @@
  * Otherwise - a pointer to the array.
  */
 char *create_array(unsigned int size, char c);
+
+/**
+ * _strdup - Returns a pointer to a newly-allocated space in memory
+ * containing a copy of the string given as parameter.
+ * @str: The string to be copied.
+ *
+ * Return: If str == NULL or insufficient memory is available - NULL.
+ * Otherwise - a pointer to the duplicated string.
+ */
+char *_strdup(char *str);
 #endif
