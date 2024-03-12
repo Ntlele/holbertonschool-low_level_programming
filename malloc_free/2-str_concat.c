124cab1cdeef9ace8e34e0d8d18742f7e6725ec6
Ntlele <ntleledavid07@gmail.com>
2024-03-12 05:45:48 +0000

    function that concatenates two strings

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,86 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+
+/**
+ * _strlen - return the length of a string
+ * @s: char type pointer
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	for (; s[i] != '\0'; i++)
+		;
+	return (i);
+}
+
+/**
+ * _strncat - funtion that concatenates strings
+ * @dest: first string
+ * @src: second string
+ * @n: value of bytes in a string
+ * Return: dest value
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int p, q;
+
+	p = 0;
+	q = 0;
+
+	while (dest[p] != '\0')
+	{
+		p++;
+	}
+	while (src[q] != '\0' && q < n)
+	{
+		dest[p] = src[q];
+		p++;
+		q++;
+	}
+	dest[p] = '\0';
+
+	return (dest);
+}
+/**
+ * str_concat - allocates space for string value
+ * @s1: pointer to destination of strin\
+ * @s2: pointer to source of string
+ * Return: pointer to copy of string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *pntr;
+	int sizef, sizes;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+
+	if (s2 == NUll)
+	{
+		s2 = "";
+	}
+	
+	sizef = _strlen(s1);
+	sizes = _strlen(s2);
+
+	ptr = malloc(((sizef + sizes) + 1) * sizeof(char));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	_strncat(ptr, s1, sizef);
+
+	_strncat(ptr, s2, sizes);
+
+	ptr += '\0';
+	return (ptr);
+}
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -20,4 +20,29 @@ char *create_array(unsigned int size, char c);
  * Otherwise - a pointer to the duplicated string.
  */
 char *_strdup(char *str);
+
+/**
+ * _strlen - return the length of a string
+ * @s: char type pointer
+ *
+ * Return: length of string
+ */
+int _strlen(char *s);
+
+/**
+ * _strncat - funtion that concatenates strings
+ * @dest: first string
+ * @src: second string
+ * @n: value of bytes in a string
+ * Return: dest value
+ */
+char _strncat(char *dest, char *src, int n);
+
+/**
+ * str_concat - allocates space for string value
+ * @s1: pointer to destination of strin\
+ * @s2: pointer to source of string
+ * Return: pointer to copy of string
+ */
+char *str_concat(char *s1, char *s2);
 #endif
