c19b8c9b020cea9eef018e144211e6c65d45b950
Ntlele <ntleledavid07@gmail.com>
2024-03-14 05:51:18 +0000

    processing...

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: pointer to first string
+ * @s2: pointer to second string
+ * @n: number of
+ * Return: pointer to allocated space
+ *
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+}
