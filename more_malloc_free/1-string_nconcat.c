0c0df1ae76eec2c0b2830f67c112d593e330b7d3
Ntlele <ntleledavid07@gmail.com>
2024-03-15 16:26:40 +0000

    unction that concatenates two strings

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -12,5 +12,35 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	char *ptr;
+	unsigned int i, j, len, sum;
 
+	for (i = 0; s[i] != '\0' && len < n; ++i)
+		len++;
+	for (j = 0; s[j] != '\0' && len < n; ++j)
+		len++;
+
+	ptr = malloc(sizeof(char) * (len + 1));
+
+	if (ptr == NULL)
+	{
+		printf("Space was not allocated\n");
+		exit (0);
+	}
+	else
+		printf("Space allocation succeded!\n\n");
+
+	i = j = 0;
+
+	for (sum = 0; sum < n; ++sum)
+	{
+		if (s1[i] != '\0')
+			ptr[sum] = s1[i++];
+		else if (s2[j] != '\0')
+			ptr[sum] = s2[j++];
+		else
+			break;
+	}
+	ptr[sum] = '\0';
+	return (ptr);
 }
--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -8,4 +8,13 @@
  */
 
 void *malloc_checked(unsigned int b);
+
+/**
+ * string_nconcat - functuion that concatenates two strings into one
+ * @s1: first string (destination)
+ * @s2: second string (source)
+ * return: pointer to the string space or NULL if fails
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 #endif
