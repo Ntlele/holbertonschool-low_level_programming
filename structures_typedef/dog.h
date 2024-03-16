2effdad252f7b396a94d2355f007298a8dd31a5a
Ntlele <ntleledavid07@gmail.com>
2024-03-16 18:32:17 +0000

    function that creates a new dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -36,30 +36,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
 
-/**
- * _strlen - returns the lenght of a string
- * @s: pointer to s
- *
- * Return: 0 on success
- *
- */
-int _strlen(char *s);
-
-/**
- * _strlen - returns the lenght of a string
- * @s: pointer to s
- *
- * Return: 0 on success
- *
- */
-int _strlen(char *s);
-
-/**
- * new_dog - creates a new dog
- * @name: name
- * @age: age
- * @owner: owner
- * Return: pointer to new struct
- */
 dog_t *new_dog(char *name, float age, char *owner);
 #endif
