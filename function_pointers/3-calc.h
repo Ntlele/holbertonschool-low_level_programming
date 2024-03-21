73c30460c9dbf945410dad750cf2504dcdba674b
Ntlele <ntleledavid07@gmail.com>
2024-03-21 18:30:41 +0000

    working process

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,4 +1,43 @@
 #ifndef CALC_H
 #define CALC_H
 
+/**
+ * op_add - sum of two integers
+ * @a: first int variable
+ * @b:second int variable
+ * returns: sum of two numbers
+ */
+int op_add(int a, int b);
+
+/**
+ * op_sub - difference of two integers
+ * @a: first int variable
+ * @b: second int variable
+ * returns: difference of two numbers
+ */
+int op_sub(int a, int b);
+
+/**
+ * op_mul - product of two integers
+ * @a: first int variable
+ * @b: second int variable
+ * returns: product of two numbers
+ */
+int op_mul(int a, int b);
+
+/**
+ * op_add - dividend of two integers
+ * @a: first int variable
+ * @b: second int variable
+ * returns: dividend of two numbers
+ */
+int op_div(int a, int b);
+
+/**
+ * op_add - division remainder of two integers
+ * @a: first int variable
+ * @b: second int variable
+ * returns: division remainder of two numbers
+ */
+int op_mod(int a, int b);
 #endif
---
+++ function_pointers/3-main.c
@@ -0,0 +1,19 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0, *operator;
+	char op;
+
+	for (; i < argc, i++)
+	{
+		argv[i];
+	}
+	
+}
