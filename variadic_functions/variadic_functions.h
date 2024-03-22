57ef333935c7f46900522f8862c02254ffe0dbcd
Ntlele <ntleledavid07@gmail.com>
2024-03-22 20:57:05 +0200

    function that prints numbers, followed by a new line

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -8,4 +8,13 @@
  */
 
 int sum_them_all(const unsigned int n, ...);
+
+/**
+ * print_numbers - prints mumbers
+ * @n: number of integers to print
+ * @separator: separates the numbers
+ *
+ * return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...);
 #endif
