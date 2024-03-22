d939121fd6996d350e7184e60667ada7c7e508cd
Ntlele <ntleledavid07@gmail.com>
2024-03-22 19:14:10 +0200

    Betty fix

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -6,8 +6,8 @@
  * @size: size of the array
  * @cmp: pointer to function of int type
  *
- * return: index of first element else -1
- *
+ * return: index of first element, otherwise -1
+ * description: returns the index of the first array element
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
