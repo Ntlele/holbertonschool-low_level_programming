20474ee0314965b24b6fe8177825625fabeae92c
Ntlele <ntleledavid07@gmail.com>
2024-03-23 19:12:58 +0200

    Betty fix

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -14,7 +14,7 @@ typedef struct dog
 {
 	char *name, *owner;
 	float age;
-}
+};
 
 /**
  * init_dog - initializes a structure called dog
@@ -36,6 +36,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
 
+typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 
 void free_dog(dog_t *d);
