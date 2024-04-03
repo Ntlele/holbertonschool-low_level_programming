0715f557ad182fe1c65645e59aaa4446b69a8020
Ntlele <ntleledavid07@gmail.com>
2024-04-03 17:14:07 +0000

    list of struct type nodes

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_list - prints the elements of a list
+ * @h: pointer to linked list
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+    size_t count = 0;
+
+    while (h != NULL)
+    {
+        if (h->str == NULL)
+            printf("[%d] (nil)\n", h->len);
+        else
+            printf("[%d] %s\n", h->len, h->str);
+
+        count++;
+        h = h->next;
+    }
+
+    return count;
+}
