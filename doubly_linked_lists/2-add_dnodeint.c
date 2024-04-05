f14e470a232ad448692d672422751fbeccf5bba5
Ntlele <ntleledavid07@gmail.com>
2024-04-05 07:18:19 +0000

    adding a node at the head of doublylist

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * add_dnodeint - adds node to the head of list
+ * @head: pointer to pointer
+ * @n: intrger value
+ * Return: new node
+ */
+dtlistint_t *add_dnodeint(dlistit_t **head, const int n)
+{
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+
+	if (*head != NULL)
+	{
+		(*head)->prev = new_node;
+	}
+
+	*head = new_node;
+	return (new_node);
+}
