cc20cefbf5044167263d3e86cf9eb3211f05a231
Ntlele <ntleledavid07@gmail.com>
2024-04-05 12:19:26 +0000

    Kernel fix

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * free_dlistint_t - frees nodes in a doubly list
+ * free_dlistint - frees nodes in a doubly list
  * @head: pointer to a node
  *
  * Return: void
