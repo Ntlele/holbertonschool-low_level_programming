8413b887fb1616177266a7343409fed5b4c3bb88
Ntlele <ntleledavid07@gmail.com>
2024-04-05 06:12:07 +0000

    Doubly lists with structures

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_dlistint - counts the number of nodes
+ * @h: pointer to the node
+ * Return: the number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h !=NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
