a31aaca96c317515bbc760b9785bc1f93fc647fe
Ntlele <ntleledavid07@gmail.com>
2024-04-05 10:33:42 +0000

    how to free nodes in a list

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_dlistint_t - frees nodes in a doubly list
+ * @head: pointer to a node
+ *
+ * Return: void
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current = head;
+	dlistint_t *next;
+	
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+}
