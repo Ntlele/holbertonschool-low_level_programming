d1fbc63315e80293d678fdf4bbbe0c4b698d547f
root <ntleledavid07@gmail.com>
2024-04-06 10:27:12 +0000

    Adding nodes and values to a doubly list

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -12,18 +12,18 @@
  * Return: new node
  */
 
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n) 
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *current = *h;
 	dlistint_t *new_node;
 	unsigned int i;
 
 	if (h == NULL)
-		return NULL;
+		return (NULL);
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
-		return NULL;
-	
+		return (NULL);
+
 	new_node->n = n;
 	if (idx == 0)
 	{
@@ -40,14 +40,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (current == NULL)
 	{
 		free(new_node);
-		return NULL;
+		return (NULL);
 	}
 	new_node->next = current->next;
 	new_node->prev = current;
-	
+
 	if (current->next != NULL)
 		current->next->prev = new_node;
 	current->next = new_node;
 
-    return new_node;
+    return (new_node);
 }
