8590fba6a0b57bd02ecf2f03c1bcb9d21befaf5e
Ntlele <ntleledavid07@gmail.com>
2024-04-06 16:28:16 +0000

    Converting a binary to an unsigned integer

---
+++ bit_minipulation/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - Prints out a variable
+ * @c: value to print
+ *
+ * Return: return 0
+ */
+int _putchar(char c);
+
+unsigned int binary_to_uint(const char *b);
+#endif
