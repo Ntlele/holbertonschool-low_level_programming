105938cacef5c4c4ba5055f6dd8b2862b6d2a94d
Ntlele <ntleledavid07@gmail.com>
2024-04-14 13:33:51 +0000

    Function that creates a file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of the file created
+ * @text_content: content to fill in the file
+ *
+ * Return: return 1 if file created or -1 if not created
+ */
+
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, count_c = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if(fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+
+		while (text_content[count_c])
+			count_c++;
+
+		write(fd, text_content, count_c);
+
+	close(fd);
+	return (1);
+}
+
+
